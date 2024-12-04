3744e8dddb8e8a65c6395096e47d0098aa775743
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-12-04 15:36:08 -0500

    More fixes to updates
    PROD-17157

--- recipes-summit/images/files/update_support.sh
+++ recipes-summit/images/files/update_support.sh
@@ -2,7 +2,15 @@
 # SPDX-License-Identifier: LicenseRef-Ezurio-Clause
 # Copyright (C) 2024 Ezurio
 
+set -o pipefail
+
 [ "${1}" = b ] && part=2 || part=1
 
-mmc bootpart enable "${part}" 1 "${2}"
-sleep 1
+timeout=10
+while [ ${timeout} -gt 0 ]; do
+    sync
+    mmc bootpart enable "${part}" 1 "${2}" >&2
+    mmc extcsd read "${2}" | grep -q "Boot Partition ${part} enabled" && break
+    sleep 1
+    timeout=$((timeout - 1))
+done
